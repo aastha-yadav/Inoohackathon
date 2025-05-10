65cefcd4b2ab3948dce3bf1b71f52d684a02b1e5
aastha-yadav <asthayadav773@gmail.com>
2025-05-10 10:50:14 +0530

    updateCode

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Page1 from './components/Page1/Page1'
 import AllRoutes from './Routes'
+import Imp from './components/Imp'
 
 const App = () => {
   return (
-    <AllRoutes/>
+    // <AllRoutes/>
+    <Imp/>
+   
+
 
   )
 }
